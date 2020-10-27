d665ced37b1cf8bd176e62520d24ce2da106f16c
nickdeans <nickabramowicz@gmail.com>
2020-10-27 15:35:25 -0700

    correction on prompt

--- js/app.js
+++ js/app.js
@@ -2,59 +2,59 @@
 
 var userName = prompt('What is your name?')
 console.log(userName);
-alert('Hi ' + userName + ', welcome!')
+alert('Hi ' + userName + ', welcome! Here are a few questions to give you insight about my interests!')
 
-var sports = prompt('Do you like sports?')
+var sports = prompt('Do I like sports?')
 console.log(sports);
 
 if(sports.toLowerCase() === 'yes'){
-    alert('I love sports too!');
+    alert('You got it! I love sports, especailly from LA!');
 } else if(sports.toLowerCase() === 'no'){
-    alert('I agree, sports are boring!')
+    alert('Nice try, but I am sure you will get the next one!')
 } else {
     alert('please answer yes or no')
 }
 
-var junkfood = prompt('Do you like junkfood?')
+var junkfood = prompt('Do I like junkfood?')
 console.log(junkfood);
 
 if(junkfood.toLowerCase() === 'yes'){
-    alert('I love junkfood too!');
+    alert('I love junkfood especially Ice Cream!');
 } else if(junkfood.toLowerCase() === 'no'){
-    alert('I agree, junkfood is gross!')
+    alert('Who doesnt like junkfood!?')
 } else {
     alert('please answer yes or no')
 }
 
-var movies = prompt('Do you like watching movies?')
+var movies = prompt('Do I like watching movies?')
 console.log(movies);
 
 if(movies.toLowerCase() === 'yes'){
-    alert('I love movies too!');
+    alert('I love movies!');
 } else if(movies.toLowerCase() === 'no'){
-    alert('I agree, movies are not cool!')
+    alert('Nice try, I am sure you will get the next one.')
 } else {
     alert('please answer yes or no')
 }
 
-var boats = prompt('Do you like boats?')
+var boats = prompt('Do I like boats?')
 console.log(boats);
 
 if(boats.toLowerCase() === 'yes'){
-    alert('I love boats too!');
+    alert('Boats are a ton of fun - especially to cruise around in the summer and fish from!');
 } else if(boats.toLowerCase() === 'no'){
-    alert('I agree, boats are not fun!')
+    alert('you got it wrong :( try again')
 } else {
     alert('please answer yes or no')
 }
 
-var candles = prompt('Do you like candles?')
+var candles = prompt('Do I like candles?')
 console.log(candles);
 
 if(candles.toLowerCase() === 'yes'){
-    alert('I love candles too!');
+    alert('Who doesnt love the smell of a good candle!?');
 } else if(candles.toLowerCase() === 'no'){
-    alert('I agree, candles are lame!')
+    alert('Maybe next time you will guess right!')
 } else {
     alert('please answer yes or no')
 }
